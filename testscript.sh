03634bfc1ac511cf280e35ed5243d96bbdb213f3
Sam Sepiol <samsepiol@somewhere.com>
2023-01-31 14:39:31 -0600

    testing the push

--- testscript.sh
+++ testscript.sh
@@ -2,3 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER" >> result.txt
+git config --global user.name "Diego"
+git config --global user.email "diego@lin.com"
