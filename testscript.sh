cf40ed3983d1d21aab2f43e478f9b4f658407ab8
Diego Pérez Gramond Muñoz <66748429+Diego-pgm@users.noreply.github.com>
2023-02-01 14:36:16 -0600

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-git config --global user.name "Diego"
-git config --global user.email "diego@lin.com"
+#git config --global user.name "Diego"
+#git config --global user.email "diego@lin.com"
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 DATE=$(date)
 echo "$DATE Result is: $NUMBER" >> result.txt
-
+files=( "./result.txt" "otherfile.txt")
+for file in "${files[@]}"
+do
+  cat file.txt
+  if [[ $? -ne 0 ]]
+  then
+    echo "file not found"
+    break
+done
 
