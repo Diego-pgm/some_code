4418b426ce177e8a3c42710899dfaef87edd9e64
Diego Pérez Gramond Muñoz <66748429+Diego-pgm@users.noreply.github.com>
2023-02-01 14:39:59 -0600

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -5,7 +5,9 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 DATE=$(date)
 echo "$DATE Result is: $NUMBER" >> result.txt
-files=( "./result.txt" "otherfile.txt" )
+declare -A files
+files[1] = "result.txt"
+files[2] = "other.tx"
 for file in "${files[@]}"
 do
   cat file.txt
